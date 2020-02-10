c9213765fba2497a532753bae46acf60158576d7
unknown <xifeng-jin>
2020-02-10 16:13:50 +0800

    知识点1

--- pages/about/about.js
+++ pages/about/about.js
@@ -5,62 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name: 'zhuling',
+    age:18,
+    students: [{id:'01',name:'kebi'},{id:'02',name:'james'},{id:'03',name:'kuli'}],
+    counter:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  addCounter() {
+    // this.data.counter ++
+    // console.log(this.data.counter)
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  subtract() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
+
 })
