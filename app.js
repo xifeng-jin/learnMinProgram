a16bb4e968a2342709e46f8c8bba9e7927534784
unknown <xifeng-jin>
2020-02-11 11:55:37 +0800

    知识5

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ App({
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
+    //这个是异步调用
     wx.getUserInfo({
       withCredentials: true,
       lang: '',
@@ -16,14 +17,19 @@ App({
   },
 
   /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   * 当小程序启动,界面显示的时候，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    
+    switch(options.scene) {
+      case 1001:
+        break;
+      case 1005:
+        break;
+    }
   },
 
   /**
-   * 当小程序从前台进入后台，会触发 onHide
+   * 当小程序从前台进入后台，界面被隐藏时，会触发 onHide
    */
   onHide: function () {
     
